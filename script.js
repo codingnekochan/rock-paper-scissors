36dfb2086d195897c27ad1b32bc825dca0460ab4
Mariam Ibrahim <ibrahimoyiza198@gmail.com>
2023-09-13 13:32:22 +0100

    refactor js file made major changes to code to enhance gaming experience and improve functionality

--- script.js
+++ script.js
@@ -1,55 +1,126 @@
-// this is to get the computer to throw its own weapon
-function getComputerChoice(){
-    let computerChoice = Math.floor(Math.random()* 3);
-    if (computerChoice === 0){
-        return "rock";   
+// score display on ui
+const playerScore = document.getElementById('player_score');
+playerScore.textContent= 0;
+const computerScore = document.getElementById('cmptr_score');
+computerScore.textContent = 0;
+//game winner display on ui
+const gameWinner = document.getElementById('rps_game--winner')
+const announcement = document.createElement('p')
+gameWinner.appendChild(announcement);
+// score initialization at start of game
+let playerCount = 0;
+let computerCount = 0;
+// for each button
+const buttonRock =document.getElementById('rock')
+const buttonPaper =document.getElementById('paper');
+const buttonScissors = document.getElementById('scissors');
+// for a single round of rock paper scissors gamer
+const roundWinner = document.getElementById('rps_rnd--winner');
+const para = document.createElement('p');
+roundWinner.appendChild(para);
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button)=>
+button.addEventListener('click',()=>{  
+    const playerSelection = getPlayerChoice()
+    const computerSelection = getComputerChoice()
+    // this code block is the value returned when player makes choice
+    function getPlayerChoice(){
+        let playerChoice = button.id;
+        return playerChoice;
     }
-    else if (computerChoice === 1){
-        return "paper";
+    // this code block selects computer choice at random
+    function getComputerChoice(){
+            let computerChoice = Math.floor(Math.random()* 3);
+        if (computerChoice === 0){
+            return "rock";   
+        }
+        else if (computerChoice === 1){
+            return "paper";
+        }
+        else {
+           return "scissors";
+        }
     }
-    else {
-       return "scissors";
+    // function block to determine winner of a round 
+    function playRound(playerSelection,computerSelection){
+        if(playerSelection === "rock"&& computerSelection ==="scissors"){
+            para.textContent= "You chose rock and Mr Computer chose scissors.You win!"
+            }
+        else if(playerSelection==="scissors"&& computerSelection==="paper"){
+            para.textContent= "You chose scissors and Mr Computer chose paper.You win!"
+        }
+        else if(playerSelection==="paper" && computerSelection==="rock"){
+            para.textContent= ("You chose paper and Mr Computer chose rock.You win!")
+        }
+        else if(playerSelection === "rock"&& computerSelection ==="paper"){
+            para.textContent= "You chose rock and Mr Computer chose paper.Mr Computer wins!"
+        }
+        else if (playerSelection==="scissors"&& computerSelection==="rock"){
+            para.textContent= "You chose scissors and Mr Computer chose rock.Mr Computer wins!"
+        }
+        else if(playerSelection==="paper" && computerSelection==="scissors"){
+            para.textContent= "You chose paper and Mr Computer chose scissors.Mr Computer wins!"
+        }
+        else {
+            para.textContent= "You both chose the same objects.It's a tie!"
+        }
     }
-}
-// the user throw weapon
-function getPlayerChoice(){
-    let player= prompt("Rock,Paper,Scissors? \n\n Pick a weapon!",);
-    const playerChoice = player.toLowerCase()       
-    return playerChoice;
-}
-// function block to determine winner of a round
-let counter = 0;      
-function playRound(playerSelection,computerSelection){
-   
-    if(
-          (playerSelection === "rock"&& computerSelection ==="scissors")
+     playRound(playerSelection, computerSelection);
+    //  this code block is to update scores;
+    // for player score
+    function playerScoreCounter(){
+        if(
+            (playerSelection === "rock"&& computerSelection ==="scissors")
         ||(playerSelection==="scissors"&& computerSelection==="paper")
         ||(playerSelection==="paper" && computerSelection==="rock")
-    ){
-        console.log("You win this round!");
-        counter++;          
+        ){
+            playerCount++; 
+            playerScore.textContent = playerCount;         
+        }
     }
-    else if(
-          (playerSelection === "rock"&& computerSelection ==="paper")
+    playerScoreCounter()
+   function computerScoreCounter(){
+        if(
+            (playerSelection === "rock"&& computerSelection ==="paper")
         ||(playerSelection==="scissors"&& computerSelection==="rock")
         ||(playerSelection==="paper" && computerSelection==="scissors")
-    ){
-        console.log("You lose this round to Mr Computer!");
+        ){
+            computerCount++; 
+            computerScore.textContent = computerCount;         
+        }
     }
-    else if (playerSelection === computerSelection){
-        console.log("It's a tie!");
-    }
-    else {
-        console.log("Wrong Input!");
-    }
-    return counter;
-}
-playRound(getPlayerChoice(),getComputerChoice());
+   computerScoreCounter()
+   // game ender
 
-// game(); 
-// if (counter >= 3){
-//     console.log("You defeated Mr Computer!")
-// }
-// else{
-//     console.log("Oops! Try again")
-// }
+   if (playerCount === 5 && computerCount < 5){
+       playerCount=0;
+       computerCount = 0;
+       buttonRock.disabled = true;
+       buttonPaper.disabled = true;
+       buttonScissors.disabled = true;
+       announcement.textContent = "Congratulations! You win! Would you like to play again?"
+   }
+   if (computerCount === 5 && playerCount < 5){
+      computerCount= 0;
+      playerCount = 0;
+      buttonRock.disabled = true;
+      buttonPaper.disabled = true;
+      buttonScissors.disabled = true;
+      announcement.textContent = "Oops! You've been defeated! Play again?"
+   }
+}));
+
+ // game reset on play again button
+function resetGame(){
+    announcement.textContent = "";
+    para.textContent = "";
+    playerScore.textContent= playerCount;
+    computerScore.textContent= computerCount;
+    buttonRock.disabled =false;
+    buttonPaper.disabled = false;
+    buttonScissors.disabled = false;
+}
+const playAgain = document.getElementById('reset')
+playAgain.addEventListener('click', resetGame);
+
+
